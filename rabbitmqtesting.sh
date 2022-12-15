085f5b0265252351c9591ddc8ec1cabc9dd2d2c5
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2022-12-15 19:51:49 +0530

    Create rabbitmqtesting.sh

---
+++ rabbitmqtesting.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
+curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
+apt-get update
+sudo apt-get install rabbitmq-server
+sudo nano /etc/default/rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
+sudo rabbitmq-plugins enable rabbitmq_mqtt
+service rabbitmq-server start
+service rabbitmq-server restart
