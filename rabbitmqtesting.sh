1b8881745d2969e8c4a44d20f0d2968e66a5a7ca
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2022-12-15 19:59:47 +0530

    Update rabbitmqtesting.sh

--- rabbitmqtesting.sh
+++ rabbitmqtesting.sh
@@ -1,3 +1,5 @@
+cd /home/iplon/repos_1
+
 sudo apt-get update
 echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
 curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
