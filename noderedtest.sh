93d2fff06857382e21a519019cef815290453844
Tamiliplon <107620306+Tamiliplon@users.noreply.github.com>
2022-12-15 11:08:38 +0530

    Update noderedtest.sh

--- noderedtest.sh
+++ noderedtest.sh
@@ -1,1 +1,9 @@
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/oss/release/grafana_9.0.7_arm64.deb
+sudo dpkg -i grafana_9.0.7_arm64.deb
 
+service grafana-server restart
+
+
+npm install -g --unsafe-perm node-red@3.0.2
+node-red
